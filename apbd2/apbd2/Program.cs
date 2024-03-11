040da583d19958ca914846ec949cddd718f6e33f
s27324 <s27324@pjwstk.edu.pl>
2024-03-11 16:23:00 +0100

    Modyfikacja 1

--- apbd2/apbd2/Program.cs
+++ apbd2/apbd2/Program.cs
@@ -1,3 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
 
-Console.WriteLine("Hello, World!");
+        int[] tbl = { 1, 2, 3, 4 };
+
+        foreach (int num in tbl)
+        {
+            Console.WriteLine(num);
+        }
+    }
+}
